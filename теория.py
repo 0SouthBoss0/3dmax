8c5b05cb42b6c2cf128ea8510149508f1deea790
Michael <mishumpel@mail.ru>
2020-11-28 17:46:58 +0300

    Теория питон

---
+++ теория.py
@@ -0,0 +1,20 @@
+# 1 2 3 4 5 -> [1, 2, 3, 4, 5]
+a = list(map(int, input().split()))
+
+# запись и чтение файла
+f = open('input.txt', 'r')
+w = open('output.txt', 'w')
+
+arr = []
+max = 0
+for line in f.readlines():
+    arr.append(int(line))
+for i in range(len(arr)):
+    if int(arr[i]) % 11 == 0 and arr[i] > max:
+        max = arr[i]
+w.write(str(max))
+f.close()
+w.close()
+
+
+
