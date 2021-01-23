5da18666690b3e14a2469c901a12947d07b76235
Michael <mishumpel@mail.ru>
2021-01-23 13:49:57 +0300

    beta

--- main.py
+++ main.py
@@ -2,38 +2,42 @@ def isInt(n):
     return int(n) == float(n)
 
 
-arr = []
+OTVET = []
 
-a = list(map(int, input().split()))
+cout = int(input())
 
-fin = a[0]
-half = a[1]
-k = a[2]
-arr.append(0)
-for i in range(fin):  # ВСЕ I + 1
-    i = i + 1
+for i in range(cout):
 
-    if i == 1:
-        arr.append(1)
-    else:
-        q = i / k
-        w = i - 1
+    arr = []
 
-        if i > half:
-            if int(q) >= half and isInt(q):
-                arr.append(arr[int(q)]+arr[w])
-            else:
-                arr.append(arr[w])
+    a = list(map(int, input().split()))
 
-        if i <= half:
-            if isInt(q):
-                arr.append(arr[int(q)] + arr[w])
-            else:
-                arr.append(arr[w])
+    fin = a[0]
+    half = a[1]
+    k = a[2]
+    arr.append(0)
+    for i in range(fin):  # ВСЕ I + 1
+        i = i + 1
 
-for i in range(len(arr)):
-    print(i, arr[i])
+        if i == 1:
+            arr.append(1)
+        else:
+            q = i / k
+            w = i - 1
 
-print("-------------")
-print("ОТВЕТ =", max(arr))
-print("-------------")
+            if i > half:
+                if int(q) >= half and isInt(q):
+                    arr.append(arr[int(q)] + arr[w])
+                else:
+                    arr.append(arr[w])
+
+            if i <= half:
+                if isInt(q):
+                    arr.append(arr[int(q)] + arr[w])
+                else:
+                    arr.append(arr[w])
+
+    OTVET.append(max(arr))
+
+for i in range(len(OTVET)):
+    print(OTVET[i])
