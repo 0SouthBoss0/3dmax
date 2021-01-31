7bf280a596630a129e5bb6ca65df7150f006ad2d
Michael <mishumpel@mail.ru>
2021-01-31 16:39:33 +0300

    alpha2

--- qwe.py
+++ qwe.py
@@ -1,42 +1,37 @@
-print("Введите количество учеников: ")
-num = int(input())
+def find_on_row(way, array, way_index, row_index):
+    if way_index == len(way):  # found answer
+        return []
+    for col_index in range(len(array[0])):
+        if array[row_index][col_index] == way[way_index]:
+            candidate_answer = find_on_col(way, array, way_index + 1, col_index)
+            if not candidate_answer is None:  # unwind answer
+                return [col_index+1] + candidate_answer
+    return None
 
-status = 0
-arr = []
-arr2 = []
-arr3 = []
-arrotvet = []
 
-# Заполнение массива с клавиатуры
-print("Введите данные учеников в формате Фамилия Имя: ")
-for i in range(num):
-    arr.append(str(input()))
+def find_on_col(way, array, way_index, col_index):
+    if way_index == len(way):  # found answer
+        return []
+    for row_index in range(len(array)):
+        if array[row_index][col_index] == way[way_index]:
+            candidate_answer = find_on_row(way, array, way_index + 1, row_index)
+            if not candidate_answer is None:  # unwind answer
+                return [row_index+1] + candidate_answer
+    return None
 
-# деление по каждому слову ("Иванов Иван" -> "Иванов", "Иван")
-for i in range(num):
-    q = arr[i]
-    q = q.split()
-    arr2.append(q[0])
-    arr2.append(q[1])
 
-# удаление имен ("Иванов", "Иван" -> "Иванов")
-for i in range(len(arr2)):
-    if i % 2 == 0:
-        arr3.append(arr2[i])
+way = []
+waylen = int(input())
+wayin = list(map(str, input().split()))
+for i in range(waylen):
+    way.append(wayin[i])
+k = int(input())
+array = [[0 for j in range(k)] for i in range(k)]
 
-# проверка на одинаковые фамилии
-for i in range(len(arr3)):
-    for j in range(len(arr3)):
-        if i != j:
-            if arr3[i] == arr3[j]:
-                status = 1
-                arrotvet.append(arr3[i])
+for i in range(k):
+    a = list(map(str, input().split()))
+    for j in range(k):
+        array[i][j] = a[j]
 
-if status == 0:
-    print("Однофамильцев нет")
-else:
-    print("Совпадающие фамилии:")
-    arrotvet = list(set(arrotvet))
+print(f'{find_on_row(way, array, 0, 0)}')
 
-for i in range(len(arrotvet)):
-    print(arrotvet[i])
